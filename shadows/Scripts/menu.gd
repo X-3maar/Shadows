98f6fcb38243c25dd71a5328f7ad058a5a3481fd
3maar1 <amarmostafaabdelhady265@gmail.com>
2026-03-01 12:05:29 +0200

    Added BGM and completed some of the menu functionality

--- shadows/Scripts/menu.gd
+++ shadows/Scripts/menu.gd
@@ -22,3 +22,8 @@ func _on_exit_pressed() -> void:
 func _on_settings_pressed() -> void:
 	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+
+
+func _on_back_pressed() -> void:
+	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
